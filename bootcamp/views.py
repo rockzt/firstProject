caa62dd6c759f8ee4d974c0ae2313e4a612012c6
rodrigo zavala <rodrigoztrejo@gmail.com>
2023-08-29 13:25:53 -0600

    [modified] list_koders.html improved UX/UI

--- bootcamp/views.py
+++ bootcamp/views.py
@@ -9,30 +9,23 @@ from django.template import loader
 # Views are functions
 # view recieve client's request
 def get_koder(request, id):
-    # Response
+
 
     koders = [
-        {"id":0, "name": "rock", "genration": "2021", "bootmcap": "python back end"},
-        {"id":1, "name": "david", "genration": "2023", "bootmcap": "front end"},
-        {"id":2, "name": "ashley", "genration": "2019", "bootmcap": "swift"},
+        {"id": 1 ,"name": "rock", "generation": "2021", "bootmcap": "python back end", "is_active": True, },
+        {"id": 2 ,"name": "david", "generation": "2023", "bootmcap": "front end", "is_active": True, },
+        {"id": 3 ,"name": "ashley", "generation": "2019", "bootmcap": "swift", "is_active": False, }
     ]
 
     find_koder = [koder for koder in koders if koder["id"] == id]
-    print(find_koder)
-    html = f"""
-    <html>
-    <body>
-       <div>
-            <h1 style="text-align:center;">Welcome koder -> {find_koder[0]["name"]}!</h><b><hr></b>
-        </div>
-        <p style="text-align:center">
-        Welcome to bootcamp app!
-        {find_koder[0]}
-        </p>
-    </body>
-    </html>
-    """
-    return HttpResponse(html)
+    context = {
+        "bootcamp": {"name": "Python", "module": "Django"},
+        "koders": find_koder
+    }
+    # Calling template
+    template = loader.get_template("list_koders.html")
+
+    return HttpResponse(template.render(context, request))
 
 
 
